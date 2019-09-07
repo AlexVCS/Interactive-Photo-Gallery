e377bba6a7f230260906a1f3f0aefc8f49d42a2f
Alex Curtis-Slep <alexcurtis-slep@Alex-Curtis-Sleps-MacBook-Pro.local>
2019-09-07 14:47:56 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ const $search = $('input[name=search]');
 $search.focus();
 console.log($search.val().toLowerCase());
 });
+
+
+for (let i = 0; i <= 11; i += 1) {
+    var caption = document.getElementsByTagName('a')[i].getAttribute('data-title').toLowerCase();
+    console.log(caption);
+}
+
+var result = caption.includes('hay', [0]);
+console.log(result);
