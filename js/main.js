59ee7c22dca50cd6c786e209f93f03d710579401
Alex Curtis-Slep <alexcurtis-slep@Alex-Curtis-Sleps-MacBook-Pro.local>
2019-09-09 18:25:42 -0400

    Remove script test

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-// $('.textbox').click(function () {
-// const $search = $('input[name=search]');
-// $search.focus();
-// console.log($search.val().toLowerCase());
-// });
-
-// for (let i = 0; i <= 11; i += 1) {
-//     var caption = document.getElementsByTagName('a')[i].getAttribute('data-title').toLowerCase();
-//     console.log(caption);
-// }
-
-
-// var result = caption.includes('hay', [0]);
-// console.log(result);
-
 document.getElementById('searchbox').addEventListener('keyup', myFunction);
 
 function myFunction () {
