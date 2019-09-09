4e63a4a78b8979938a28d19cb32178bb89e74732
Alex Curtis-Slep <alexcurtis-slep@Alex-Curtis-Sleps-MacBook-Pro.local>
2019-09-09 11:08:49 -0400

    Working on search function

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function myFunction () {
     ul = document.getElementById('lesPhotos');
     li = document.getElementsByTagName('li');
 
-    for (let i = 0; i <= 11; i += 1) {
+    for (let i = 0; i < li.length; i ++) {
         a = li[i].getElementsByTagName('a')[0];
         caption = a.textContent || a.innerText;
         if (document.getAttribute('data-title').indexOf(filter) > -1) {
