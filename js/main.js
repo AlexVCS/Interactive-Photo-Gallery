657dc900a54fa70db916b8aec6a6c93d26b6539b
Alex Curtis-Slep <alexcurtis-slep@Alex-Curtis-Sleps-MacBook-Pro.local>
2019-09-09 18:23:53 -0400

    Update search function!

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@
 // var result = caption.includes('hay', [0]);
 // console.log(result);
 
+document.getElementById('searchbox').addEventListener('keyup', myFunction);
 
 function myFunction () {
     var input, filter, ul, li, a, i, caption;
@@ -23,8 +24,8 @@ function myFunction () {
 
     for (let i = 0; i < li.length; i ++) {
         a = li[i].getElementsByTagName('a')[0];
-        caption = a.textContent || a.innerText;
-        if (document.getAttribute('data-title').indexOf(filter) > -1) {
+        caption = a.getAttribute('data-title') || a.innerText;
+        if (caption.toLowerCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
